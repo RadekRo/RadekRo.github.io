f6601debf744ef22f39088fb7ae1d1152d63d1fd
Radosław Rocławski <r.roclawski@ankara.pl>
2018-03-05 07:14:35 +0100

    Created projects done windows. Added linkedIn and Github to bio

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
+const $page = $('html, body');
 const $skillsProgressBar = $('.progress .progress-bar');
+const $webPages = $('.webpages-container img');
+const $scrollToTopBtn = $('.return-to-top');
 
 function animateProgressBars() {
     $skillsProgressBar
@@ -10,4 +13,32 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 });
 
+function webPagesFadeIn() {
+
+    scrollY>50 ?
+        $webPages
+            .css('opacity', '1')
+            .css('transition', 'all 2.0s ease-in-out') :
+            null;
+}
+
+function showScrollToTopButton() {
+
+    $(window).scrollTop() > 200 ?
+        $scrollToTopBtn.fadeIn(300).css('display', 'flex') :
+        $scrollToTopBtn.css('display', 'none');
+
+}
+
+function scrollToTop() {
+
+    const animationTime = 700;
+    $page.animate({ scrollTop: 0}, animationTime);
+
+}
+
 $(document).on('ready', animateProgressBars());
+$(window).on('scroll', webPagesFadeIn);
+$(window).on('scroll', showScrollToTopButton);
+$scrollToTopBtn.on('click', scrollToTop);
+
